07019da584522b62cc25325b1d0927b6e341445f
Marcin Majchrzak <marcin.majchrzak@10clouds.com>
2017-08-25 11:37:06 +0200

    Use correct args.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
 var TokenExchange = artifacts.require("./TokenExchange.sol");
 
 module.exports = function(deployer) {
-    deployer.deploy(TokenExchange);
+    deployer.deploy(
+        TokenExchange,
+        0x71544d4D42dAAb49D9F634940d3164be25ba03Cc, // admin
+        0x71544d4D42dAAb49D9F634940d3164be25ba03Cc, // fee account
+        // Rest of settings is taken from production
+        // https://etherscan.io/address/0x8d12a197cb00d4747a1fe03395095ce2a5cc6819#readContract
+        0x0000000000000000000000000000000000000000, // levels account
+        0, // fee make
+        3000000000000000, // fee take
+        0 // fee rebate
+    );
 };
